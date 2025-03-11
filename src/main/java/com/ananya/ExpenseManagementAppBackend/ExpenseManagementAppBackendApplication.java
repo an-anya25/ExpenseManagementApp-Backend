002b4102f8c7c2c8eaffe9660b867d6a69b977e4
an_anya25 <ananyababu25@gmail.com>
2025-03-11 23:32:47 +0530

    first commit

---
+++ src/main/java/com/ananya/ExpenseManagementAppBackend/ExpenseManagementAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ananya.ExpenseManagementAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagementAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseManagementAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ananya/ExpenseManagementAppBackend/ExpenseManagementAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ananya.ExpenseManagementAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagementAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
